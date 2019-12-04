bf38918fc8d604f77f45f200768429a23d5c63ec
Matt Whitesides <mbwxd4@mail.missouri.edu>
2019-12-04 17:18:02 -0600

    HW6 Done

--- Homework/6/Untitled-2.py
+++ Homework/6/Untitled-2.py
@@ -1,27 +1,39 @@
-class Graph(object):
-    def __init__(self, size):
-        self.adjMatrix = []
-        for i in range(size):
-            self.adjMatrix.append([0 for i in range(size)])
-        self.size = size
-    def addEdge(self, v1, v2):
-        if v1 == v2:
-            print("Same vertex %d and %d" % (v1, v2))
-        self.adjMatrix[v1][v2] = 1
-        self.adjMatrix[v2][v1] = 1
-    def removeEdge(self, v1, v2):
-        if self.adjMatrix[v1][v2] == 0:
-            print("No edge between %d and %d" % (v1, v2))
-            return
-        self.adjMatrix[v1][v2] = 0
-        self.adjMatrix[v2][v1] = 0
-    def containsEdge(self, v1, v2):
-        return True if self.adjMatrix[v1][v2] > 0 else False
-    def __len__(self):
-        return self.size
+class Graph(): 
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.G = [[0 for y in range(vertices)] for x in range(vertices)] 
+  
+    def print_mst(self, pi): 
+        for i in range(1, self.V):
+            if pi[i] is not None:
+                print("Edge: ({0},{1}), Weight: {2}".format(pi[i], i, self.G[i][pi[i]]))
+  
+    def extract_min(self, key, Q): 
+        min = sys.maxsize 
+  
+        for v in range(self.V): 
+            if key[v] < min and Q[v] == 0: 
+                min = key[v] 
+                min_index = v 
+  
+        return min_index 
+
+    def mst_prim(self):
+        key = [sys.maxsize] * self.V 
+        pi = [None] * self.V 
+
+        key[0] = 0 
+        Q = [0] * self.V 
+  
+        pi[0] = -1
         
-    def toString(self):
-        for row in self.adjMatrix:
-            for val in row:
-                print('{:4}'.format(val)),
-            print
+        for v in range(self.V): 
+            u = self.extract_min(key, Q) 
+            Q[u] = 1
+            for v in range(self.V): 
+                if self.G[u][v] > 0 and Q[v] == 0 and key[v] > self.G[u][v]: 
+                    pi[v] = u
+                    key[v] = self.G[u][v] 
+ 
+  
+        self.print_mst(pi)
